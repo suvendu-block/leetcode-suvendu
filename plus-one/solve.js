9744ffa8c05383dab69838ce3241636f9c5cc6ec
suvendu-block <forexsuvendu@gmail.com>
2026-08-18 20:47:45 +0530

    next6

---
+++ plus-one/solve.js
@@ -0,0 +1,14 @@
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+
+    // If all digits were 9
+    digits.unshift(1);
+    return digits;
+};
