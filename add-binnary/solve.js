f8b94dc0c29144f2b96635c3c994ee260681660f
suvendu-block <forexsuvendu@gmail.com>
2026-08-19 21:48:32 +0530

    next7

---
+++ add-binnary/solve.js
@@ -0,0 +1,25 @@
+var addBinary = function(a, b) {
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = [];
+
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = carry;
+
+        if (i >= 0) {
+            sum += Number(a[i]);
+            i--;
+        }
+
+        if (j >= 0) {
+            sum += Number(b[j]);
+            j--;
+        }
+
+        result.push(sum % 2);
+        carry = Math.floor(sum / 2);
+    }
+
+    return result.reverse().join('');
+};
